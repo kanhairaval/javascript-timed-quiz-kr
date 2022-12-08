cda7088cfe243222fbc133f92a79034e725d0690
kanhairaval <kanhairaval@gmail.com>
2022-12-08 01:33:56 -0500

    rendering questions

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -3,16 +3,30 @@ let startButton = document.getElementById("strt-button");
 let leftHeader = document.getElementById("high-score");
 let rightHeader = document.getElementById("time-left");
 let timeLeft = 60;
+let questionsStart = document.getElementById("questions");
 
-let quizQuestions = ["Which of the following methods is used to access HTML elements using Javascript?", "How can a datatype be declared to be a constant type?", "Which of the following keywords is used to define a variable in Javascript?", "Which function is used to serialize an object into a JSON string in Javascript?", "How to stop an interval timer in Javascript?", "How do we write a comment in javascript?"];
+let quizQuestions = [
+    {question: "Which of the following function of String object returns the calling string value converted to upper case?", answers: [{text: "toLocaleUpperCase()", correct: false}, {text: "toUpperCase()", correct: true}, {text: "toString()", correct: false}, {text: "substring()", correct: false}]}, 
+    {question: "Inside which HTML element do we put the JavaScript?", answers: [{text: "<javascript>", correct: false}, {text: "<js>", correct: false}, {text: "<scripting>", correct: false}, {text: "<script>", correct: true}]},
+    {question: "How do you write Hello World in an alert box?", answers: [{text: "msgBox('Hello World')", correct: false}, {text: "alert('Hello Wolrd')", correct: true}, {text: "msg('Hello World')", correct: false}, {text: "alertBox('Hello World')", correct: false},]},
+    {question: "How to write an IF statement in JavaScript?", answers: [{text: "if (i === 5)", correct: true}, {text: "if i == 5 then", correct: false}, {text: "if i = 5 then", correct: false}, {text: "if i = 5", correct: false}]},
+    {question: "How to write an IF statement for executing some code if i is NOT equal to 5?", answers: [{text: "if (i != 5)", correct: true}, {text: "if i =! 5 then", correct: false}, {text: "if i <> 5", correct: false}, {text: "if (i <> 5)", correct: false}]},
+    {question: "How does a FOR loop start?", answers: [{text: "for (i <= 5; i++)", correct: false}, {text: "for (i = 0; i <= 5)", correct: false}, {text: "for (i = 0; i <= 5; i++)", correct: true}, {text: "for i = 1 to 5", correct: false}]}];
 
 let questionsVar = document.getElementById("questions-container");
 
-// questionsVar.setAttribute("Style", "display: none");
+questionsVar.setAttribute("Style", "display: none");
 
-// let startQuiz = startButton.addEventListener("click", function() {
-//     document.getElementById("initial-container").style.display = "none";
-//     if (questionsVar.setAttribute("Style", "display: none") === true) {
-//         questionsVar.setAttribute("Style", "display: block");
-//     }
-// })
+let startQuiz = startButton.addEventListener("click", function() {
+    document.getElementById("initial-container").style.display = "none";
+    if (questionsVar.style.display === "none") {
+        renderQuestions();
+    }
+});
+
+let renderQuestions = function () {
+    questionsVar.style.display = "block"
+    for (let i = 0; i <= quizQuestions.length; i++) {
+        questionsStart[this.question.textContent] = quizQuestions[Math.floor(Math.random() * 6)]
+    }
+}
